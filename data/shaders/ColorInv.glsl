88b9f8e41930189a2c5a296fc28aa6168b195bea
0b1001100 <90949174+0b1001100@users.noreply.github.com>
2022-01-01 09:40:00 +0900

    Update ColorInv.glsl

--- data/shaders/ColorInv.glsl
+++ data/shaders/ColorInv.glsl
@@ -2,7 +2,7 @@ uniform sampler2D tex;
 uniform vec2 resolution;
 
 void main(void){
-  vec2 pos=vec2(gl_FragCoord.x/resolution.x,gl_FragCoord.y/resolution.y);
+  vec2 pos=vec2(gl_FragCoord.xy)/vec2(resolution.xy);
   vec4 color=vec4(texture2D(tex,vec2(pos.x,1.0-pos.y)));
-  gl_FragColor = vec4(1.0 - color.x, 1.0 - color.y, 1.0 - color.z, 1.0);
+  gl_FragColor = vec4(vec3(1.0) - vec3(color.xyz), 1.0);
 }
